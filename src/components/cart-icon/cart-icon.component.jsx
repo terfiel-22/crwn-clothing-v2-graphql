9d51ad554ce5e54d13ea1ffc708e306d59cb43ca
terfiel-22 <dietherfiel@gmail.com>
2023-10-04 12:19:56 +0800

    GrapQL starting point

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+
+import ShoppingIcon from "../../assets/shopping-bag.svg";
+
+import { CartContext } from "../../contexts/cart.context";
+
+import { CartIconContainer, ItemCount } from "./cart-icon.styles";
+
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  return (
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <img src={ShoppingIcon} alt="ShoppingIcon" className="shopping-icon" />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
+  );
+};
+
+export default CartIcon;
